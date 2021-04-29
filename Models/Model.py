d5fd549dde0468c282a4bc81fdb4c2056ca28d85
zithachitra <>
2021-04-29 09:51:36 +0000

    update

--- Models/Model.py
+++ Models/Model.py
@@ -9,84 +9,53 @@ from tensorflow import keras as KerasModel
 from util_yaml import yaml_dump, yaml_loader
 from tensorflow.keras.optimizers import SGD
 
-
 DIRNAME = Path(__file__).parents[1].resolve() / "weights"
 
 
 class Model():
-	def __init__(
-		self,
-		dataset_cls: type,
-		network_fn: Callable[..., KerasModel],
-		net_args: Dict,
-		net_io_shapes: Dict,
-		dataset_args: Dict = None,
-	):
-		self.name = f"{self.__class__.__name__}_{dataset_cls.__name__}_{network_fn.__name__}"
+    def __init__(
+        self,
+        dataset_cls: type,
+        network_fn: Callable[..., KerasModel],
+        net_args: Dict,
+        net_io_shapes: Dict,
+        dataset_args: Dict = None,
+    ):
+        self.name = f"{self.__class__.__name__}_{dataset_cls.__name__}_{network_fn.__name__}"
 
-		if dataset_args == None:
-			dataset_args = {}
-		self.data = dataset_cls(**dataset_args)
-		self.data.load_or_generate()
-		self.data.preprocess()
+        if dataset_args == None:
+            dataset_args = {}
+        self.data = dataset_cls(**dataset_args)
+        self.data.load_or_generate()
+        self.data.preprocess()
 
+        self.net_args = net_args
+        self.network = network_fn(num_encoder_tokens=self.data.io_shapes[0],
+                                  num_decoder_tokens=self.data.io_shapes[1])
 
-		
-		self.net_args = net_args
-		self.network = network_fn(net_args, net_io_shapes)
+    @property
+    def weights_filename(self) -> str:
+        DIRNAME.mkdir(parent=True, exist_ok=True)
+        return str(DIRNAME / f"{self.name}_weights.h5")
 
-	
-	@property
-	def weights_filename(self)->str:
-		DIRNAME.mkdir(parent=True, exist_ok=True)
-		return str(DIRNAME / f"{self.name}_weights.h5")
+    def fit(self, callbacks):
+        config = self.net_args
 
-	
-	def fit(
-		self,
-		callbacks
-	):
-		config = self.net_args
+        sgd = SGD(lr=config["learn_rate"],
+                  decay=config["decay"],
+                  momentum=config["momentum"],
+                  nesterov=True)
 
-		sgd = SGD(lr=config["learn_rate"], decay=config["decay"], momentum=config["momentum"],
-          nesterov=True)
+        # if callbacks in None:
+        # 	callbacks = []
 
-		# if callbacks in None:
-		# 	callbacks = []
+        self.network.compile(loss="categorical_crossentropy",
+                             optimizer=sgd,
+                             metrics=["accuracy"])
 
-		self.network.compile(
-			loss="categorical_crossentropy",
-			optimizer=sgd,
-			metrics=["accuracy"]
-		)
-
-		self.network.fit(
-			[self.data.encoder_input_data, self.data.decoder_input_data],
-			self.data.decoder_target_data,
-			validation_split=0.2,
-			epochs = config["epochs"],
-			callbacks=callbacks
-		)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        self.network.fit(
+            [self.data.encoder_input_data, self.data.decoder_input_data],
+            self.data.decoder_target_data,
+            validation_split=0.2,
+            epochs=config["epochs"],
+            callbacks=callbacks)
--- util_yaml.py
+++ util_yaml.py
@@ -3,6 +3,7 @@ import yaml
 # data = yaml.load(file_descriptor)
 # yaml.dump(data)
 
+
 def yaml_loader(filepath):
     """ Loads data from a yaml file """
     with open(filepath, "r") as file_descriptor:
@@ -17,35 +18,29 @@ def yaml_dump(filepath, data):
 
 
 if __name__ == "__main__":
-	
-	data = {
-		"project_name": "cnn_mnist",
-		"dataset": {
-			"name": "Mnist",
-			"dataset_args": {
-				"img_width": None,
-				"img_height": None
-			}
-		},
-		"network": {
-			"name": "nn_conv1",
-			"net_args": {
-				"decay": 1.0e-06,
-				"dropout": 0.2,
-				"epochs": 20,
-				"hidden-layer-size": 128,
-				"layer-1-size": 16,
-				"layer-2-size": 32,
-				"learn-rate": 0.01,
-				"momentum": 0.9
-			},
-			"io_shapes": {
-				"input_shape": None,
-				"output_shape": None
-			}
-		}
-	}
 
-	yaml_dump("experiment.yaml", data)
-	print("Done!")
+    data = {
+        "project_name": "ManyThings",
+        "dataset": {
+            "name": "ManyThings",
+            "dataset_args": None
+        },
+        "network": {
+            "name": "nn_lstm1",
+            "net_args": {
+                "decay": 1.0e-06,
+                "dropout": 0.2,
+                "epochs": 20,
+                "latent_dim": 300,
+                "learn-rate": 0.01,
+                "momentum": 0.9
+            },
+            "io_shapes": {
+                "input_shape": None,
+                "output_shape": None
+            }
+        }
+    }
 
+    yaml_dump("experiment.yaml", data)
+    print("Done!")
